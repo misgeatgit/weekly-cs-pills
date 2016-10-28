0c22eca2123b6b370419efeeda232715aca9ee4f
Misgana Bayetta <misgana.bayetta@gmail.com>
2016-10-28 16:08:11 +0800

    more fixes

--- code.ai/neural_net.rb
+++ code.ai/neural_net.rb
@@ -10,7 +10,7 @@ Author Misgana Bayetta <misgana.bayetta@gmail.com> 2016
 class Utility
   # A sigmoid function is  1/(1+e^-x)
   def self.sigmoid x = [], w = []
-    1/(1+ Math.exp(-1 * dotp(x,w)))
+    1.0/(1.0 + Math.exp(-1.0 * dotp(x,w)))
   end
 
   def self.dotp x = [], w = []
@@ -154,36 +154,41 @@ class NeuralNet
 
 
   # Main method to start the training. 
-  def train
+  def train max_epoch
     epoch = 0
-    until epoch < 5000 do
-      epoch = epoch + 1
-      layers = @hidden_layers + @output_layer
-      # Forward propagate.
-      # i.e compute outputs based on input and current estimated weight vector 
-      # starting from hidden layer one to the final output layer.
-      for i in  0..layers.size()-2
-        outputs = layers[i].compute_output()
-        layers[i+1].set_input(outputs)
-      end
+    until epoch < max_epoch do
+      for d in 0..@X.size()-1
+        epoch = epoch + 1
+        layers = @hidden_layers + @output_layer
+        # Forward propagate.
+        # i.e compute outputs based on input and current estimated weight vector 
+        # starting from hidden layer one to the final output layer.
+        layers[0].input = @X[d]
+        outputs = layers[0].compute_output()
+        for i in  1..layers.size()-2
+          layers[i].input = outputs
+          outputs = layers[i].compute_output()
+        end
 
-      # Juz making sure values are updated. No refs in ruby AFAIK.
-      @output_layer = layers.pop()
-      @hidden_layers = layers
+        # Juz making sure values are updated. No refs in ruby AFAIK.
+        @output_layer = layers.pop()
+        @hidden_layers = layers
 
-      # Compute hypothesis
-      @output_layer.compute_output()
+        # Compute hypothesis
+        @output_layer.compute_output()
 
-      # Backward propagate.
-      # i.e compute error on the output layer and propagate adjustment values back
-      # the the hidden units iteratively till the first hidden unit.
-      sigma = sigma_output()
-      i=@hidden_layers.size()-1
-      while i >= 0 do
-        sigma = @hidden_layers[i].compute_sigma(sigma,@utput_layer.weights) #TODO add output vec
+        # Backward propagate.
+        # i.e compute error on the output layer and propagate adjustment values back
+        # the the hidden units iteratively till the first hidden unit.
+        sigma = @output_layer.compute_sigma()
+        @ouput_layer.update_weights()
+        i=@hidden_layers.size()-1
+        while i >= 0 do
+          sigma = @hidden_layers[i].compute_sigma(sigma,@utput_layer.weights)
+          @hidden_layers[i].update_weights()
+        end
       end
     end
-
   end
 
   #TODO Add/remove hidden layers and units dynamically
